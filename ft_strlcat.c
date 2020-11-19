0cc4f3359871de874e2f10b77c40781b6a017720
sshakya <sshakya@student.42.fr>
2020-11-19 18:56:12 +0100

    strlcat fixed norm almost done

---
+++ ft_strlcat.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sshakya <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/18 17:40:32 by sshakya           #+#    #+#             */
+/*   Updated: 2020/11/19 18:53:44 by sshakya          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+static	int	ft_strlen(const char *s)
+{
+	unsigned int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+int		ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	s;
+	size_t	d;
+
+	d = ft_strlen(dst);
+	i = d;
+	if (size == 0)
+		return (ft_strlen(src));
+	if (size < d)
+		return (size + ft_strlen(src));
+	s = 0;
+	if (size > i)
+		while (src[s] && size - i > 1)
+		{
+			dst[i] = src[s];
+			i++;
+			s++;
+		}
+	dst[i] = '\0';
+	while (src[s] != '\0')
+		s++;
+	return (d + s);
+}
