7af4c3f7ad42b1ee3208234a579c875638a2b0a7
sshakya <sshakya@student.42.fr>
2020-11-21 04:20:55 +0100

    strncmp norm done

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: sshakya <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/21 03:37:54 by sshakya           #+#    #+#             */
-/*   Updated: 2020/11/21 03:47:54 by sshakya          ###   ########.fr       */
+/*   Updated: 2020/11/21 04:20:30 by sshakya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,12 @@ int	strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-
 	if (n == 0)
 		return (0);
 	while (i < n)
 	{
 		if (s1[i] == s2[i])
-		i++;
+			i++;
 		return (s2 - s1);
 	}
 	return (0);
