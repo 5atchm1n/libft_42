28d5663c483b20f7472878562f375cb70ad6c36d
sshakya <sshakya@student.42.fr>
2020-11-21 04:19:59 +0100

    strnstr done with norm check & strncmp.c done missing norm

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -1,36 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sshakya <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/20 20:40:34 by sshakya           #+#    #+#             */
-/*   Updated: 2020/11/21 04:18:45 by sshakya          ###   ########.fr       */
+/*   Created: 2020/11/21 03:37:54 by sshakya           #+#    #+#             */
+/*   Updated: 2020/11/21 03:47:54 by sshakya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
-char		*ft_strnstr(const char *s1, const char *s2, size_t len)
+int	strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
-	size_t	j;
 
 	i = 0;
-	j = 0;
-	if (!s2)
-		return ((char *)s1);
-	while (*s1 && i < len)
+
+	if (n == 0)
+		return (0);
+	while (i < n)
 	{
-		if (*s1 == *s2)
-		{
-			while (*s2 && *(s1 + j) == *(s2 + j))
-				j++;
-			return ((char *)s1);
-		}
-		s1++;
+		if (s1[i] == s2[i])
 		i++;
+		return (s2 - s1);
 	}
-	return (NULL);
+	return (0);
 }
