af18065d5b553b8da0ace7dc8e974d5a3edcedb9
sshakya <sshakya@student.42.fr>
2020-11-24 22:14:08 +0100

    added itoa

---
+++ ft_itoa.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sshakya <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/24 00:16:17 by sshakya           #+#    #+#             */
+/*   Updated: 2020/11/24 04:32:14 by sshakya          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+static int	ft_power_ten(int i)
+{
+	int	len;
+
+	len = 1;
+	while (i > 9)
+	{
+		i = i / 10;
+		len++;
+	}
+	return (len);
+}
+
+static void	ft_prepend(char c, char *str, int len)
+{
+	if (len != 0)
+		str[len - 1] = c;
+	if (len == 0)
+		str[len] = c;
+}
+
+char	*ft_itoa(int n)
+{
+	int	i;
+	int	len;
+	char	c;
+	char	*ret;
+
+	len = ft_power_ten(n);
+	if (!(ret = malloc(sizeof(int) * (len + 1))))
+		return (NULL);
+	ret[len] = '\0';
+	i = n;
+	while (i > 9)
+	{
+		c = (i % 10) + '0';
+		ft_prepend(c, ret, len);
+		i = i / 10;
+		len--;
+	}
+	c = (i % 10) + 48;
+	ft_prepend(c, ret, len);
+	return (ret);
+}
