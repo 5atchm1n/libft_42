99e38d1be9e24bd0d7f6b36ae45a1028371f7ef9
sshakya <sshakya@student.42.fr>
2020-11-26 03:00:52 +0100

    update libft with bonus functions

--- libft.h
+++ libft.h
@@ -6,20 +6,21 @@
 /*   By: sshakya <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/21 04:22:05 by sshakya           #+#    #+#             */
-/*   Updated: 2020/11/24 22:07:12 by sshakya          ###   ########.fr       */
+/*   Updated: 2020/11/26 00:31:29 by sshakya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+# include <unistd.h>
+# include <stdlib.h>
 
 #ifndef LIBFT_H
 # define LIBFT_H
-# include <unistd.h>
-# include <stdlib.h>
+
 char	**ft_split(char const *str, char c);
-char	*ft_calloc(size_t num, size_t size);	
 char	*ft_itoa(int n);
 char	*ft_strchr(const char *str, int c);
 char	*ft_strdup(const char *s);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 char	*ft_strnstr(const char *s1, const char *s2, size_t len);
 char	*ft_strrchr(const char *str, int c);
 char	*ft_strtrim(char const *str, char const *set);
@@ -35,14 +36,32 @@ int	ft_strlcat(char *dst, const char *src, size_t size);
 int	ft_strncmp(const char *s1, const char *s2, size_t n);
 int	ft_tolower(int c);
 int	ft_toupper(int c);
-size_t	ft_strlcpy(char *dest, char *src, size_t size);
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 size_t	ft_strlen(const char *s);
-void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
+void	*ft_calloc(size_t num, size_t size);	
+void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, int c, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
 void	*memcpy(void *dest, const void *src, size_t n);
-void	bzero(void *s, size_t n);
+void	ft_bzero(void *s, size_t n);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putstr_fd(char *s, int fd);
+
+typedef struct		s_list
+{
+	void		*content;
+	struct s_list	*next;
+}				t_list
+
+int	ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
+t_list	*ft_lstnew(void *content);
+void	*ft_lstadd_back(t_list **lst, t_list *new);
+void 	*ft_lstadd_front(t_list **lst, t_list *new);
+
 #endif
 
