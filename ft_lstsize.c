607d21faf9bc308423608e2fa10523a3ff4ee4c2
sshakya <sshakya@student.42.fr>
2020-11-29 09:57:13 +0100

    :heavy_check_mark: because I am silly
    *
    
    Left some comments in ft_lstsize
    
    *

--- ft_lstsize.c
+++ ft_lstsize.c
@@ -6,27 +6,12 @@
 /*   By: sshakya <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/25 23:12:20 by sshakya           #+#    #+#             */
-/*   Updated: 2020/11/28 23:33:55 by sshakya          ###   ########.fr       */
+/*   Updated: 2020/11/29 09:56:46 by sshakya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-/*
-int	ft_lstsize(t_list *lst)
-{
-	int size;
 
-	size = 0;
-	if (lst->next == NULL)
-		return (1);
-	while (lst != NULL)
-	{
-		lst++;
-		size++;
-	}
-	return (size);
-}
-*/
 int	ft_lstsize(t_list *lst)
 {
 	if (lst == NULL)
