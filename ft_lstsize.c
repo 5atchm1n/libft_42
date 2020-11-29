fe7a6fbdf30fd8f2bf11e2de8e1082728f9eeec6
sshakya <sshakya@student.42.fr>
2020-11-29 10:18:23 +0100

    really.. finally norm done
    *
    
    *

--- ft_lstsize.c
+++ ft_lstsize.c
@@ -6,7 +6,7 @@
 /*   By: sshakya <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/25 23:12:20 by sshakya           #+#    #+#             */
-/*   Updated: 2020/11/29 09:56:46 by sshakya          ###   ########.fr       */
+/*   Updated: 2020/11/29 10:17:41 by sshakya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,5 @@ int	ft_lstsize(t_list *lst)
 		return (0);
 	if (lst->next == NULL)
 		return (1);
-	
 	return (1 + ft_lstsize(lst->next));
 }
-
